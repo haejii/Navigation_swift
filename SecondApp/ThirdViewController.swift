ddd347126c809c4cbade606795e293fec54ff52d
haejii <yanghagi@naver.com>
2021-08-23 17:20:29 +0900

    Implement Notification

--- SecondApp/ThirdViewController.swift
+++ SecondApp/ThirdViewController.swift
@@ -5,11 +5,16 @@ class ThirdViewController: UIViewController {
     
     var thirdViewControllerData:String?
 
+   
     override func viewDidLoad() {
+       
         super.viewDidLoad()
 
         thirdViewControllerData = "ThiredViewData"
     }
     
 
+    @IBAction func btn(_ sender: Any) {
+        NotificationCenter.default.post(name:Notification.Name("ChangeBackColor"), object: nil, userInfo: nil)
+    }
 }
